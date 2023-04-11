5b3896ba1e3cd33a7a8e9a41f6d3996c83728377
imani.devs <imaniroberts.dev@gmail.com>
2023-04-11 14:52:46 -0400

    created counter

---
+++ src/Components/Visitors.js
@@ -0,0 +1,18 @@
+import React from 'react' 
+import { useState } from 'react' // This is a hook
+
+const Visitors = () => {
+    const [count, setCount] = useState(0); 
+    const incrementCount = () => {
+        setCount(count + 1);
+    }
+
+    return (
+    <div>
+    <h1>Counter: {count}</h1>
+    <button onClick={incrementCount}>Click Me!</button>
+    </div>
+)
+}
+
+export default Visitors
