7e83444d58450c34c6b564825b2a2f8c42809fc7
imani.devs <imaniroberts.dev@gmail.com>
2023-04-12 22:00:25 -0400

    update header

--- src/Components/Average_Rating.js
+++ src/Components/Average_Rating.js
@@ -5,8 +5,7 @@ const Average_Rating = () => {
   <h2>4.6</h2>
   return (
     <div className='AvgRating'>
-      <h2>Average Rating</h2>
-      <h2>4.6</h2>
+      <h2>Average Rating: 4.6</h2>
     </div>
   )
 }
